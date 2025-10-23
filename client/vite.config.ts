4801209a7bbd96ab5ab9d4d98e70bc7d10ef92e6
pranav <pedaprolusaipranav@gmail.com>
2025-10-23 20:43:32 +0530

    Added .gitignore to clean up repo

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Important for GitHub Pages when the site is served under /<repo-name>/
-  base: '/Document_-Editor/',
+  base: '/Document-Editor/',
   server: {
     host: '0.0.0.0',
     port: 5175,
